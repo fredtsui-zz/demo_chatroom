7aafc29e6301d4c33d1eb66d2130f1b9871514ea
fredtsui <yizhi.tsui@gmail.com>
2018-01-11 21:34:38 -0500

    init commit

---
+++ src/main/java/com/example/demo/DemoChatroomApplication.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.kurento.client.KurentoClient;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableWebSocket
+public class DemoChatroomApplication implements WebSocketConfigurer{
+	
+	@Bean
+	public UserRegistry registry() {
+	    return new UserRegistry();
+	}
+
+	@Bean
+	public RoomManager roomManager() {
+	    return new RoomManager();
+	}
+
+	@Bean
+	public ChatRoomHandler groupCallHandler() {
+	    return new ChatRoomHandler();
+	}
+
+	@Bean
+	public KurentoClient kurentoClient() {
+	    return KurentoClient.create();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoChatroomApplication.class, args);
+	}
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+	    registry.addHandler(groupCallHandler(), "/groupcall");
+	}
+}
